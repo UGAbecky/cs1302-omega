b20756ec322888bbd8004c625daa157992dbc8c5
Becky Lee <sl39085@uga.edu>
2021-05-10 04:00:01 -0400

    score counter fixed

--- src/main/java/cs1302/omega/OmegaApp.java
+++ src/main/java/cs1302/omega/OmegaApp.java
@@ -34,8 +34,8 @@ public class OmegaApp extends Application {
     private static final int PLAYER_BARY = 75;
 
     //constants for the window dimensions
-    private static final int width = 1000;
-    private static final int height = 600;
+    private static final int WIDTH = 1000;
+    private static final int HEIGHT = 600;
 
     //constant for the ball dimension
     private static final double BALL = 20;
@@ -43,12 +43,12 @@ public class OmegaApp extends Application {
     //variables
     int ballYSpeed = 1;
     int ballXSpeed = 1;
-    double xBallSpot = width / 2;
-    double yBallSpot = height / 2;
+    double xBallSpot = WIDTH / 2;
+    double yBallSpot = HEIGHT / 2;
     DemoGame game;
     Canvas canvas;
-    double yPlayer1 = height / 2;
-    double yPlayer2 = height / 2;
+    double yPlayer1 = HEIGHT / 2;
+    double yPlayer2 = HEIGHT / 2;
     Label notice;
     ImageView banner;
     int player1Score = 0;
@@ -56,7 +56,7 @@ public class OmegaApp extends Application {
     Image bannerImage;
     boolean gameOn;
     int xPlayer1 = 0;
-    double xPlayer2 = width - PLAYER_BARX;
+    double xPlayer2 = WIDTH - PLAYER_BARX;
     VBox root;
     Timeline screenPlay;
 
@@ -76,7 +76,7 @@ public class OmegaApp extends Application {
         banner.setFitWidth(640);
         Label notice = new Label ("Modify the starter code to suit your needs.");
         Label instructions = new Label ("Move left/right with arrow key; click rectangle" +
-        "to teleport.");
+            "to teleport.");
         DemoGame game = new DemoGame(640,240);
         VBox root = new VBox(banner, notice, instructions, game);
         Scene scene = new Scene(root);
@@ -88,20 +88,27 @@ public class OmegaApp extends Application {
         stage.sizeToScene();
         stage.show();
 
-        //background size
-		Canvas canvas = new Canvas(width, height);
-		GraphicsContext screen = canvas.getGraphicsContext2D();
+        //orienting new background
+        Canvas canvas = new Canvas(WIDTH, HEIGHT);
+        GraphicsContext screen = canvas.getGraphicsContext2D();
 
-        Timeline screenPlay = new Timeline(new KeyFrame(Duration.millis(1), e ->
-        gamePlay(screen)));
-		screenPlay.setCycleCount(Timeline.INDEFINITE);
+        //creating a new timeline for the game to run on
+        Timeline screenPlay = new Timeline(new KeyFrame(Duration.millis(10), e ->
+            gamePlay(screen)));
+        /*
+          we want the timeline to continue as long as the game is in motion so the game can execute
+          gracefully.
+         */
+        screenPlay.setCycleCount(Timeline.INDEFINITE);
 
-		//mouse control (move and click)
+        //mouse control (move and click)
         canvas.setOnMouseMoved(e ->  yPlayer1  = e.getY());
-		canvas.setOnMouseClicked(e ->  gameOn = true);
-		stage.setScene(new Scene(new StackPane(canvas)));
-		stage.show();
-		screenPlay.play();
+
+        //when the mouse is clicked, the button for "start game" is triggered.
+        canvas.setOnMouseClicked(e ->  gameOn = true);
+        stage.setScene(new Scene(new StackPane(canvas)));
+        stage.show();
+        screenPlay.play();
 
     } // start
 
@@ -112,6 +119,7 @@ public class OmegaApp extends Application {
      */
 
     private void playerLoss() {
+        // conditions for when the user loses.
         if (xBallSpot < xPlayer1 - PLAYER_BARX) {
             player2Score++; //increasing score by 1
             gameOn = false;
@@ -125,6 +133,7 @@ public class OmegaApp extends Application {
      */
 
     private void computerLoss() {
+        // condition for when the computer loses, user wins.
         if (xBallSpot > xPlayer2 + PLAYER_BARX) {
             player1Score++; // increasing score by 1
             gameOn = false;
@@ -137,7 +146,8 @@ public class OmegaApp extends Application {
      */
 
     private void ballInBounds() {
-        if  (yBallSpot < 0 || yBallSpot > height) {
+        // checks if the pong ball is within the board dimensions.
+        if  (yBallSpot < 0 || yBallSpot > HEIGHT || xBallSpot < 0 || xBallSpot > WIDTH) {
             ballYSpeed *= -1;
         }
     } // ballinBounds
@@ -147,10 +157,10 @@ public class OmegaApp extends Application {
      */
 
     private void ballSpeed() {
-        gameOn = true;
         if (((xBallSpot + BALL > xPlayer2) && yBallSpot >= yPlayer2 &&
-        yBallSpot <= yPlayer2 + PLAYER_BARY) || ((xBallSpot < xPlayer1 + PLAYER_BARX)
-        && yBallSpot >= yPlayer1 && yBallSpot <= yPlayer1 + PLAYER_BARY)) {
+            yBallSpot <= yPlayer2 + PLAYER_BARY) || ((xBallSpot < xPlayer1 + PLAYER_BARX)
+            && yBallSpot >= yPlayer1 && yBallSpot <= yPlayer1 + PLAYER_BARY)) {
+            //increasing speed here
             ballYSpeed += 1 * Math.signum(ballYSpeed);
             ballXSpeed += 1 * Math.signum(ballXSpeed);
             ballYSpeed *= -1;
@@ -164,7 +174,9 @@ public class OmegaApp extends Application {
      */
 
     private void ballTrack() {
+        //equaling the position of the ball to the velocity it is traveling in y direction
         yBallSpot += ballYSpeed;
+        //equaling the position of the ball to the velocity it is traveling in x direction
         xBallSpot += ballXSpeed;
     } // ballTrack
 
@@ -174,11 +186,14 @@ public class OmegaApp extends Application {
      */
 
     private void restart() {
-        ballXSpeed = new Random().nextInt(2) == 0? 1: -1;
-        ballYSpeed = new Random().nextInt(2) == 0? 1: -1;
+        //velocity of the ball is returned to normal in x direction
+        ballXSpeed = new Random().nextInt(2) == 0 ? 1 : -1;
+        // velocity of the ball is returned to normal in y direction
+        ballYSpeed = new Random().nextInt(2) == 0 ? 1 : -1;
 
-        yBallSpot = height / 2;
-        xBallSpot = width / 2;
+        // the pong ball is returned to its original location, in the middle of the board.
+        yBallSpot = HEIGHT / 2;
+        xBallSpot = WIDTH / 2;
     }
 
     /**
@@ -192,38 +207,53 @@ public class OmegaApp extends Application {
      */
 
     private void gamePlay (GraphicsContext screen) {
+        //background screen for the board will be black.
         screen.setFill(Color.BLACK);
-        screen.fillRect(0,0,width,height);
+        screen.fillRect(0,0,WIDTH, HEIGHT);
+
+        //the details of the score, the bar, and the ball are white.
         screen.setFill(Color.WHITE);
         screen.setFont(Font.font(30));
 
-        if(gameOn) {
+        //while the game loop in invoked, we initiate the {@code ballTrack} for ball movement
+        if (gameOn) {
             ballTrack();
-            if (xBallSpot < width - width  / 4) {
-				yPlayer2 = yBallSpot - PLAYER_BARY / 2;
-			}  else {
-				yPlayer2 =  yBallSpot > yPlayer2 + PLAYER_BARY /
-                    2 ?yPlayer2 += 1: yPlayer2 - 1;
-			}
+            //setting up computer user
+            if (xBallSpot < WIDTH - WIDTH  / 4) {
+                yPlayer2 = yBallSpot - PLAYER_BARY / 2;
+            }  else {
+                yPlayer2 =  yBallSpot > yPlayer2 + PLAYER_BARY /
+                    2 ? yPlayer2 += 1 : yPlayer2 - 1;
+            }
             screen.fillOval(xBallSpot, yBallSpot, BALL, BALL);
         } else {
-            //if the game is currently not in play, a click button is available to start the game
+            /*if the game is currently not in play, the user can stroke
+              his/her mouse to begin the game.
+             */
             screen.setStroke(Color.WHITE);
+            // aligning the new stroke text to the center of the board.
             screen.setTextAlign(TextAlignment.CENTER);
-
             //set the start game text in the middle of the screen
-			screen.strokeText("Start Game", width / 2, height / 2);
+            screen.strokeText("Start Game", WIDTH / 2, HEIGHT / 2);
             restart(); // setting the game with the original game conditions
         }
+        /*
+          we call in the other methods to ruun the gamePlay method.
+          the program takes into consideration the loss sitations, and accomodating
+          the program to a quicker ball speed when one of the users are not losing.
+        */
         ballInBounds();
         playerLoss();
         computerLoss();
         ballSpeed();
 
-        //displaying score
-        screen.fillText(player1Score + "\t\t\t\t\t\t\t\t\t\t" + player2Score, width / 2, 100);
-		screen.fillRect(xPlayer2, yPlayer2, PLAYER_BARX, PLAYER_BARY);
-		screen.fillRect(xPlayer1, yPlayer1, PLAYER_BARX, PLAYER_BARY);
+        /*
+          the score will be displayed throughout the entire game and will count up by incraments
+          of one for the winner of that round.
+        */
+        screen.fillText(player1Score + "\t\t\t\t\t\t\t\t\t\t" + player2Score, WIDTH / 2, 100);
+        screen.fillRect(xPlayer2, yPlayer2, PLAYER_BARX, PLAYER_BARY);
+        screen.fillRect(xPlayer1, yPlayer1, PLAYER_BARX, PLAYER_BARY);
 
     } // gamePlay
 } //OmegaApp
